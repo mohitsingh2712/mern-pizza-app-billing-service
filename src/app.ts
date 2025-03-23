1b65dcb7f84d1a30b56512d2929414a5b9c104ee
mo14337 <mohitsingh14336@gmail.com>
2025-03-23 17:22:54 +0530

    add cors policy

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,19 @@ import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import customerRouter from "./customer/customerRouter";
 import couponRouter from "./coupon/couponRouter";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "http://localhost:5173"],
+        credentials: true,
+    }),
+);
 
 //routes
 app.use("/customer", customerRouter);
