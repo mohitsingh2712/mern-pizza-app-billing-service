63e10d6240667a84b4b67b64b271f2f6ebb1e291
mo14337 <mohitsingh14336@gmail.com>
2025-03-29 22:19:23 +0530

    Add kafka methods

---
+++ src/config/kafka.ts
@@ -0,0 +1,33 @@
+import { Consumer, EachMessagePayload, Kafka } from "kafkajs";
+import { MessageBroker } from "../types/broker";
+
+export class KafkaBroker implements MessageBroker {
+    private consumer: Consumer;
+    constructor(clientId: string, brokers: string[]) {
+        const kafka = new Kafka({ clientId, brokers });
+        this.consumer = kafka.consumer({ groupId: clientId });
+    }
+    async connectConsumer() {
+        await this.consumer.connect();
+    }
+    async disconnectConsumer() {
+        await this.consumer.disconnect();
+    }
+    async consumeMessage(topic: string, fromBegining: boolean = false) {
+        await this.consumer.subscribe({ topic, fromBeginning: fromBegining });
+        await this.consumer.run({
+            eachMessage: async ({
+                topic,
+                partition,
+                message,
+            }: EachMessagePayload) => {
+                // eslint-disable-next-line no-console
+                console.log({
+                    topic,
+                    partition,
+                    value: message.value!.toString(),
+                });
+            },
+        });
+    }
+}
