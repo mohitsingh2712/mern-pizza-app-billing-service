9e03e7886f1cadc7800a0908ad21e94f391f55e7
mo14337 <mohitsingh14336@gmail.com>
2025-03-29 22:25:55 +0530

    Added singleton broker instance and connected to broker

---
+++ src/common/factories/brokerFactory.ts
@@ -0,0 +1,14 @@
+import { KafkaBroker } from "../../config/kafka";
+import { MessageBroker } from "../../types/broker";
+import config from "config";
+let broker: MessageBroker | null = null;
+export const createMessageBroker = (): MessageBroker => {
+    if (!broker) {
+        // making this instance singleton
+        broker = new KafkaBroker(
+            "billing-service",
+            config.get("kafka.brokers"),
+        );
+    }
+    return broker;
+};
--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,21 @@ import app from "./app";
 import { initDb } from "./config/db";
 import logger from "./config/logger";
 import config from "config";
+import { MessageBroker } from "./types/broker";
+import { createMessageBroker } from "./common/factories/brokerFactory";
 
 const startServer = async () => {
     const PORT: number = config.get("server.port") || 5503;
+    let broker: MessageBroker | null = null;
     try {
         await initDb();
+        broker = createMessageBroker();
+        await broker.connectConsumer();
         app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
     } catch (err: unknown) {
+        if (broker) {
+            await broker.disconnectConsumer();
+        }
         if (err instanceof Error) {
             logger.error(err.message);
             logger.on("finish", () => {
