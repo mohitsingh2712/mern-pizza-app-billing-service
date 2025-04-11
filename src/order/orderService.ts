4c141b1e1894ab042b3383ed9875be3568b410a6
mo14337 <mohitsingh14336@gmail.com>
2025-04-11 21:01:30 +0530

    fix order  update query

--- src/order/orderService.ts
+++ src/order/orderService.ts
@@ -10,7 +10,7 @@ export class OrderService {
         return await OrderModel.create([order], { session });
     }
     async updateOrderPaymentStatus(id: string, status: string) {
-        return await OrderModel.updateOne(
+        return await OrderModel.findOneAndUpdate(
             { _id: id },
             { paymentStatus: status },
             { new: true },
