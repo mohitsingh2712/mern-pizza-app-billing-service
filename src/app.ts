bfab0e1fd4e394d5d457d49065f679e30fb7a2b3
mo14337 <mohitsingh14336@gmail.com>
2025-05-04 20:40:50 +0530

    updated cors

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,23 @@ import express from "express";
 import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import config from "config";
 //routes
 import customerRouter from "./customer/customerRouter";
 import couponRouter from "./coupon/couponRouter";
 import orderRouter from "./order/orderRouter";
 import paymentRouter from "./payment/paymentRouter";
 const app = express();
-
+const allowedOrigins = [
+    config.get("frontend.client"),
+    config.get("frontend.admin"),
+];
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["http://localhost:3000", "http://localhost:5173"],
+        origin: allowedOrigins as string[],
         credentials: true,
     }),
 );
